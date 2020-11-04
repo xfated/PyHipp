6b5646c9204b5ea9378b68e7c59d8d37ea67906d
Kai Yang <taykaiyang556@gmail.com>
2020-11-04 20:35:43 +0800

    my stuff

--- PyHipp/waveform.py
+++ PyHipp/waveform.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import hickle as hkl
 import os
 import numpy as np
+from .misc import getChannelInArray
 
 class Waveform(DPT.DPObject):
     # Please change the class name according to your needs
@@ -52,9 +53,6 @@ class Waveform(DPT.DPObject):
         # The following is some hints of the things-to-do:
         
         # read the mountainsort template files
-        # .........................................
-        # ..................code...................
-        # .........................................
         # check on the mountainsort template data and create a DPT object accordingly
         # Example:
         if len(self.data) > 0:
@@ -73,9 +71,7 @@ class Waveform(DPT.DPObject):
         for ar in wf.array_dict:
             self.array_dict[ar] = self.numSets
         self.numSets += 1
-        # .........................................
-        # ..................code...................
-        # .........................................
+
         
     def plot(self, i = None, ax = None, getNumEvents = False, getLevels = False,\
              getPlotOpts = False, overlay = False, **kwargs):
@@ -103,17 +99,27 @@ class Waveform(DPT.DPObject):
 
         if getPlotOpts:  # this will be called by PanGUI.main to obtain the plotOpts to create a menu once we right-click on the axis
             return plotOpts 
-
+            
+        if self.current_plot_type is None:
+            self.current_plot_type = plot_type
+            
         if getNumEvents:  
             # this will be called by PanGUI.main to return two values: 
             # first value is the total number of items to pan through, 
             # second value is the current index of the item to plot
-            # .........................................
-            # ..................code...................
-            # .........................................
-            
+            if self.current_plot_type == plot_type:
+                if plot_type == 'Channel':
+                    return self.numSets, i
+                elif plot_type == 'Array':
+                    return len(self.array_dict), i
+            elif self.current_plot_type == 'Array' and plot_type == 'Channel':
+                self.current_plot_type = plot_type
+                return self.numSets, i
+            elif self.current_plot_type == 'Channel' and plot_type == 'Array':
+                self.current_plot_type = plot_type
+                return len(self.array_dict), i
             return  # please return two items here: <total-number-of-items-to-plot>, <current-item-index-to-plot>
-                
+        
         if ax is None:
             ax = plt.gca()
 
@@ -123,31 +129,54 @@ class Waveform(DPT.DPObject):
         ######################################################################
         #################### start plotting ##################################
         ######################################################################
-        if plot_type == 'Channel':  # plot in channel level
-            # plot the mountainsort data according to the current index 'i'
-            # .........................................
-            # ..................code...................
-            # .........................................
-            pass  # you may delete this line
+        fig = ax.figure # get the parent figure of the ax
+        if plot_type == 'Channel':
+            if self.current_plot_type == 'Array':
+                self.remove_subplots(fig)
+                ax = fig.add_subplot(1,1,1)
+            self.plot_data(i, ax, plotOpts, isCorner = True)
+        elif plot_type == 'Array':
+            self.remove_subplots(fig)
+            advals = np.array([*self.array_dict.values()])
+            # set the starting index cstart for array i
+            # set the ending index cend for array i
+            if i == 0:
+                currch = 0
+            else:
+                currch = advals[i-1] + 1
+            cend = advals[i]
+            while currch <= cend :
+                # get channel name
+                currchname = self.dirs[currch]
+                # get axis position for channel
+                ax, isCorner = getChannelInArray(currchname, fig)
+                self.plot_data(currch, ax, plotOpts, isCorner)
+                currch += 1
+            self.current_plot_type = plot_type
+        return ax
     
+    def plot_data(self, i, ax, plotOpts, isCorner):
+        y = self.data[i]
+        x = np.arange(y.shape[0])
+        ax.plot(x,y)
+        
         ########labels###############
         if not plotOpts['TitleOff']:  # if TitleOff icon in the right-click menu is clicked
             # set the title in this format: channelxxx, fill with zeros if the channel number is not three-digit
-            # .........................................
-            # ..................codes..................
-            # .........................................
-            pass  # you may delete this line
+            ax.set_title(self.dirs[i])
             
-        if not plotOpts['LabelsOff']:  # if LabelsOff icon in the right-click menu is clicked
+        if not plotOpts['LabelsOff'] or isCorner:  # if LabelsOff icon in the right-click menu is clicked
             # set the xlabel and ylabel
-            # .........................................
-            # ..................code...................
-            # .........................................
-            pass  # you may delete this line
-            
-        return ax
+            ax.set_xlabel('Time (sample unit)')
+            ax.set_ylabel('Voltage (uV)')
+        
+        if plotOpts['TicksOff'] or (not isCorner):
+            ax.set_xticklabels([])
+            ax.set_yticklabels([])
     
-    
+    def remove_subplots(self, fig):
+        for x in fig.get_axes(): # remove all axes in current figure
+            x.remove()
     
     #%% helper functions        
     # Please make use of the properties of the OOP to call and edit the field-value
